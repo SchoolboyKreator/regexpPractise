492a3ef7eda94c9e9a8db2d9792fd34fa74ebf8a
Schloolboy <dmitryshtefirtsa@gmail.com>
2022-09-27 02:50:58 +0300

    ss

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 let input = document.querySelector(".input");
 let output = document.querySelector(".output");
 const regexp = new RegExp('#\\w+');
+const exclude = new RegExp('^\\#');
+
+const exclude2 = new RegExp('\\*\\*');
+const exclude2after = new RegExp('\\**$');
 const regexp2 = new RegExp('^\\*\\*\\w+\\*\\*');
 
 function translate() {
-    console.log(regexp);
-    console.log(regexp2);
+
     const string = input.value;
     output.innerHTML = input.value;
 
    if (regexp.test(string)) {
-   output.innerHTML = `<h1>${input.value}</h1> `;
+   output.innerHTML = `<h1>${input.value.replace(exclude, '')}</h1> `;
+   console.log(input.value)
    } 
 
    if (regexp2.test(string)) {
-    output.innerHTML = `<b>${input.value}</b> `
+    output.innerHTML = `<b>${input.value}</b> `; 
    }
 
    
